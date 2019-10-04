52b91d6a616955056e7b19654cd742adf5aeaaab
sjeremich23 <51142646+sjeremich23@users.noreply.github.com>
2019-10-04 10:34:30 -0600

    Tab component finished

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+	.get("https://lambda-times-backend.herokuapp.com/topics")
+	.then(response => {
+		// deal with the response data in here
+		console.log(response);
+		const trendingTopics = document.querySelector(".topics");
+		response.data.topics.forEach(item => {
+			let topics = tabContent(item);
+			trendingTopics.append(topics);
+		});
+	})
+	.catch(err => {
+		// deal with the error in here
+		console.log(err);
+	});
+
+function tabContent(dataresponse) {
+	let newDiv = document.createElement("div");
+	newDiv.textContent = dataresponse;
+	newDiv.classList.add("tab");
+	return newDiv;
+}
